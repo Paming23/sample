631363d196160b09f2c326f401ea0c06edce2e6c
Paming23 <pamingbang@gmail.com>
2025-05-14 22:36:20 +0800

    Add files via upload

---
+++ word-list.js
@@ -0,0 +1,10 @@
+const wordList = [
+	{
+        word: "banana",
+        hint: "Bulaklak Muna Ang Gawin Bago Ito Kainin."
+    },
+	{
+		word: "Shadow",
+		hint: "Kahit Anung Gawin Mo Na Malayo Nakasunod Parin Sayo"
+	},
+];
